07429ce23166987ec8b68e6b4373847b86414e11
huda <you@example.com>
2020-02-05 13:11:00 +0300

    complete

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # TicTacToe
-#Technologies
+
+-This is a simple game which has created by JavaScript  
+for two players  .
+#####click here to play the game.
+
+###Technologies:
 -HTML5
 -CSS
 -JavaScript
 -DOM
-#wireframes 
-..........
 
-#Something to add
+###Link to wireframes:
+-[wireframes link]https://wireframe.cc/AzEudv
+
+###Something to add :
 -Allow 2 players to play online with each other 
 -Add more themes.
--Add more breakpoints.
 
-I craeat array for user clicks on a box the clicking function executes. 
-This function changes the value of the  array index then loop through the array to check if someone won. whin's find someone won it will stop the playing and it will add the theme for wining.
+###How it works:
+-The user choose X or O and clicks on a box if their clicks in a horizontal, vertical, or diagonal will wins the game.
 
 
-he player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
--- game.js
+++ game.js
@@ -2,25 +2,32 @@ console.log('Hello from JS!');
 
 
 let turn = true; //true means X turn
-const squares = [];
+const squares = [];//
 let counter = 0;//how many pressd
 let gamecheck = false;
 
 
 
-
-let box = document.querySelectorAll('.box');
-
-
-function selectwinner(b1,b2,b3)
-{
-b1.style.background = 'grey';
-b2.style.background = 'grey';
-b3.style.background = 'grey';
-
-who.innerHTML= 'you Win the game';
+const button = document.querySelector('.reset');//button in class reset
+let box = document.querySelectorAll('.box');//select all boxs
+button.addEventListener('click', playagame);
 
 
+
+
+
+function playagame() {
+
+    for (let i = 0; i < box.length; i++) {
+
+        box[i].style.background = 'rgb(49, 33, 47)';
+        box[i].innerHTML = '';
+        box[i].addEventListener('click', clickEvent);
+    }
+    who.innerHTML = 'Play';
+    counter = 0;
+    gamecheck = false;
+    //console.log(gamecheck);
 }
 
 function clickEvent() {
@@ -29,113 +36,102 @@ function clickEvent() {
 
         this.innerHTML = "X";//chang value to be x
         turn = false;
-        who.innerHTML = "O Turn"; 
+        who.innerHTML = "O Turn";
 
     } else if (!turn && this.innerHTML == "") {
         this.innerHTML = "O";
         turn = true;
-        who.innerHTML = "X Turn"; 
+        who.innerHTML = "X Turn";
 
     }
     winner();
     counter = counter + 1;
 }
 
-
-
-
 function winner() {
 
+    // check horizontal................................
 
-
-    // check horizontal
     if (box[0].innerHTML == box[1].innerHTML && box[1].innerHTML == box[2].innerHTML && box[0].innerHTML) {
-        box[0].innerHTML 
+        box[0].innerHTML
         gamecheck = true;
-        selectwinner(box[0],box[1],box[2]);
+        selectwinner(box[0], box[1], box[2]);
 
     }
     else if (box[3].innerHTML == box[4].innerHTML && box[4].innerHTML == box[5].innerHTML && box[3].innerHTML) {
-     box[3].innerHTML 
+        box[3].innerHTML
         gamecheck = true;
-        selectwinner(box[3],box[4],box[5]);
+        selectwinner(box[3], box[4], box[5]);
 
     }
     else if (box[6].innerHTML == box[7].innerHTML && box[7].innerHTML == box[8].innerHTML && box[6].innerHTML != "") {
-     box[6].innerHTML 
+        box[6].innerHTML
         gamecheck = true;
-        selectwinner(box[6],box[7],box[8]);
+        selectwinner(box[6], box[7], box[8]);
 
     }
 
-    //check vertical
+    //check vertical....................................
+
     else if (box[0].innerHTML == box[3].innerHTML && box[3].innerHTML == box[6].innerHTML && box[0].innerHTML != "") {
-        box[0].innerHTML 
+        box[0].innerHTML
         gamecheck = true;
-        selectwinner(box[0],box[3],box[6]);
+        selectwinner(box[0], box[3], box[6]);
 
     }
     else if (box[1].innerHTML == box[4].innerHTML && box[4].innerHTML == box[7].innerHTML && box[1].innerHTML != "") {
-     box[1].innerHTML 
+        box[1].innerHTML
         gamecheck = true;
-        selectwinner(box[1],box[4],box[7]);
+        selectwinner(box[1], box[4], box[7]);
 
     }
     else if (box[2].innerHTML == box[5].innerHTML && box[5].innerHTML == box[8].innerHTML && box[2].innerHTML != "") {
-        box[3].innerHTML 
+        box[3].innerHTML
         gamecheck = true;
-        selectwinner(box[2],box[5],box[8]);
+        selectwinner(box[2], box[5], box[8]);
 
     }
 
-    //check diagonal
+    //check diagonal.........................................
+
     else if (box[0].innerHTML == box[4].innerHTML && box[4].innerHTML == box[8].innerHTML && box[0].innerHTML != "") {
-     box[0].innerHTML 
+        box[0].innerHTML
         gamecheck = true;
-        selectwinner(box[0],box[4],box[8]);
+        selectwinner(box[0], box[4], box[8]);
 
     }
     else if (box[2].innerHTML == box[4].innerHTML && box[4].innerHTML == box[6].innerHTML && box[2].innerHTML != "") {
-         box[2].innerHTML 
+        box[2].innerHTML
         gamecheck = true;
-        selectwinner(box[2],box[4],box[6]);
+        selectwinner(box[2], box[4], box[6]);
 
     }
     //if no one wins 
     else {
         if (counter == 8)
-        selectwinner( 'tie');
+            selecttie();
     }
-    //............................................
 
+    function selectwinner(b1, b2, b3) {
+        b1.style.background = 'grey';
+        b2.style.background = 'grey';
+        b3.style.background = 'grey';
+        who.innerHTML = 'you Win the game';
 
+    }
+
+    function selecttie() {
+
+        if (counter == 8) {
+
+            who.innerHTML = 'tie';
+        }
+    }
     if (gamecheck) {
         clearEvents();
     }
 }
-
-
-for (let i = 0; i < box.length; i++) {
-    box[i].addEventListener('click', clickEvent);
-
-}
-
-
-function playagame() {
-
-    for (let i = 0; i < box.length; i++) {
-        box[i].style.background = 'rgb(49, 33, 47)';
-
-        box[i].innerHTML = '';
-        box[i].addEventListener('click', clickEvent);
-    }
-    counter = 0;
-    gamecheck = false;
-    //console.log(gamecheck);
-}
-
-
-
+//clear events
 function clearEvents() {
 
     for (let i = 0; i < box.length; i++) {
@@ -143,8 +139,3 @@ function clearEvents() {
 
     }
 }
-
-
-const button = document.querySelector('.reset');
-button.addEventListener('click', playagame);
-
